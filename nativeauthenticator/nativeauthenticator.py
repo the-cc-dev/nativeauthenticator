b04f63d97e7db1206bf88e19840c7550d0cdb9e5
Leticia Portella <leportella@gmail.com>
2019-01-18 17:23:26 +0000

    Add check if user exceeded attempt of logins

--- nativeauthenticator/nativeauthenticator.py
+++ nativeauthenticator/nativeauthenticator.py
@@ -1,10 +1,12 @@
 import bcrypt
+from datetime import datetime
 from jupyterhub.orm import User
 from jupyterhub.auth import Authenticator
 
 from sqlalchemy import inspect
 from sqlalchemy.orm import relationship
 from tornado import gen
+from traitlets import Integer
 
 from .handlers import (AuthorizationHandler, ChangeAuthorizationHandler,
                        SignUpHandler)
@@ -13,9 +15,16 @@ from .orm import UserInfo
 
 class NativeAuthenticator(Authenticator):
 
+    allowed_failed_logins = Integer(
+        config=True,
+        default=0,
+        help="""shudhsud"""
+    )
+
     def __init__(self, add_new_table=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.login_attempts = dict()
         if add_new_table:
             self.add_new_table()
 
@@ -25,13 +34,36 @@ class NativeAuthenticator(Authenticator):
             User.info = relationship(UserInfo, backref='users')
             UserInfo.__table__.create(self.db.bind)
 
+    def exceed_atempts_of_login(self, username):
+        now = datetime.now()
+        login_attempts = self.login_attempts.get(username)
+        if not login_attempts:
+            self.login_attempts[username] = {'count': 1, 'time': now}
+            return False
+
+        time_last_attempt = now - login_attempts['time']
+        if time_last_attempt.seconds > 60:
+            self.login_attempts.pop(username)
+            return False
+
+        if login_attempts['count'] < 3:
+            self.login_attempts[username]['count'] += 1
+            self.login_attempts[username]['time'] = now
+            return False
+
+        return True
+
     @gen.coroutine
     def authenticate(self, handler, data):
-        user = UserInfo.find(self.db, data['username'])
+        username = data['username']
+        password = data['password']
+
+        user = UserInfo.find(self.db, username)
         if not user:
             return
-        if user.is_authorized and user.is_valid_password(data['password']):
-            return data['username']
+
+        if user.is_authorized and user.is_valid_password(password):
+            return username
 
     def get_or_create_user(self, username, password):
         user = User.find(self.db, username)
